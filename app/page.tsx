d9dcb2b34bcfafd148f259c6ae1fe8b858497138
zennon-sml <zennonsampaioml@gmail.com>
2024-07-11 17:06:04 -0300

    feat: jwt img

--- app/page.tsx
+++ app/page.tsx
@@ -5,19 +5,18 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-2 items-center">
       <Image
-        src="/catch.png"
+        src="/jwtbw.png"
         alt="Gotta Catch'em all"
-        width={500}
-        height={200}
-        blurDataURL="https://i.kym-cdn.com/entries/icons/original/000/028/484/raw.png"
+        width={280}
+        height={280}
+        blurDataURL="/jwt.png"
         placeholder="blur"
       />
       <h1 className="bg-green-500 rounded-md p-2">
-        Homepage, You can access this page without auth, but you can't get them
-        all
+        Homepage, You can access this page without auth
       </h1>
       <Link href={'/protected'} className="bg-lime-500 rounded-md p-2">
-        Protected Route(this one you need auth and you can get them all)
+        Protected Route
       </Link>
     </div>
   )
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -5,16 +5,14 @@ export default function Protected() {
   return (
     <div className="flex flex-col bg-amber-500 rounded-md p-2 items-center">
       <Image
-        src={'/gen2starters.png'}
+        src="/jwt.png"
         alt="Gotta Catch'em all"
-        width={400}
-        height={180}
+        width={280}
+        height={280}
         blurDataURL="https://i.kym-cdn.com/entries/icons/original/000/028/484/raw.png"
         placeholder="blur"
       />
-      <p>
-        This is a route Protected by JWT token, and congrats you got them all
-      </p>
+      <p>This is a route Protected by JWT token</p>
     </div>
   )
 }
